c933cc4586936b4569600eee82fd10edd9d10c75
Dasio <dasiodev@gmail.com>
2015-05-14 13:39:55 +0200

    More commands to CLI

--- src/game/main.cpp
+++ src/game/main.cpp
@@ -66,24 +66,87 @@ void drawMap(Game &game)
     std::cout << std::endl << "Free stone: " << stoneToString(stone) << std::endl;
 }
 
+void printCardsAndPlayers(Game &game)
+{
+    std::array<std::string,4> players;
+    int N = game.labyrinth.getSize();
+    Stone stone;
+    for(int y=1; y<=N; y++)
+    {
+        for(int x=1; x<=N; x++)
+        {
+            stone = game.labyrinth.get(y,x);
+            if(stone.treasure)
+                std::cout << "Card " << stone.treasure << " on " << x << ":" << y << std::endl;
+            for(int i =0;i<4;i++)
+            {
+                if (stone.player_slots[i] != nullptr)
+                    players[i] = std::to_string(x) + ":" + std::to_string(y);
+            }
+
+        }
+    }
+    std::cout << std::endl;
+    for(int i =0;i<4;i++)
+    {
+        if(!players[i].empty())
+            std::cout << game.getPlayerName(i) << " is on " << players[i] << std::endl;
+    }
+}
+
 int main(/*int argc, char *argv[]*/)
 {
     std::string command;
+    std::string name;
     Game game;
     int x,y;
-    game.addPlayer("Player1");
-    game.addPlayer("Player2");
+    int size = 7;
+    int treasures = 12;
 
-    if (!game.initialize())
-        return 1;
+    while(!std::cin.eof())
+    {
+        std::cin >> command;
+        if(command == "exit")
+        {
+            return 0;
+        }
+        else if(command == "add")
+        {
+            std::cin >> name;
+            game.addPlayer(name);
+        }
+        else if(command == "set")
+        {
+            std::cin >> command;
+            if(command == "mapsize")
+                std::cin >> size;
+            else if(command == "treasures")
+                std::cin >> treasures;
+        }
+        else if (command == "create")
+        {
+            if(!game.initialize(size,treasures))
+                std::cout << "Failed to init game";
+            else
+                break;
+        }
+        else
+        {
+            std::cout << "Unknown command";
+        }
+    }
     while(!std::cin.eof())
     {
         drawMap(game);
-        std::cout << "On turn Player" << game.activePlayer() + 1;
+        std::cout << std::endl;
+        printCardsAndPlayers(game);
+        std::cout << "Actual card: " << game.actualCard() << std::endl;
+        std::cout << "On turn " << game.getPlayerName(game.activePlayer());
         if(game.nextAction() == MOVE)
             std::cout << "MOVE" << std::endl;
         else
             std::cout << " SHIFT" << std::endl;
+        std::cout << std::endl;
         std::cin >> command;
         if(command == "exit")
         {
